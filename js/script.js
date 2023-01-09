6ea404a00db181a971c464d727d26a3b00884e92
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-01-09 19:13:53 +0100

    cambios script

--- js/script.js
+++ js/script.js
@@ -1,25 +1,50 @@
+const URL = 'https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json';
 
-const arrayVacio = [];
-const arrayVacio2 = [];
-
-function getJSON() {
-  fetch('https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json')
-    .then(res => res.json())
-    .then(data => {
-      arrayVacio
-      let prueba = data.map(questions => {
-        return {
-          "question": questions.question,
-          "answers": questions.answers,
-          "correct": questions.correct
-        }
-      })
-      arrayVacio.push(prueba)
-    })
+async function getJSON() {
+  try {
+    const response = await fetch(URL);
+    return await response.json();
+  } catch (error) {
+    console.error(message.error)
+  }
 }
 
-getJSON()
+async function iterarArray() {
+  const data = await getJSON();
+  const question = data.map((element, i) => {
+    return {
+      "question": element.question,
+      "answer": element.answers,
+      "correct": element.correct,
+      "index": i
+    }
+  });
+  return question;
+}
 
-console.log(arrayVacio);
-console.log(arrayVacio2);
+async function generateQuestions() {
+  const array = await iterarArray();
 
+  const questionsIndex = array.find(element => element.index === 0);
+
+  if (questionsIndex) {
+    const questionTitle = document.querySelector('main h1');
+    questionTitle.innerHTML = questionsIndex.question;
+
+    const answerTitle = document.querySelector('main button');
+    answerTitle.innerHTML = questionsIndex.answer;
+
+  }
+}
+
+generateQuestions();
+
+async function getPeticion() {
+  const response = await fetch('https://raw.githubusercontent.com/Cjavierlopez/REP-DeliveriesHAB/main/projectONE/questions-images/question01.png');
+  const image = await response.blob();
+
+  const imagen = document.querySelector('main img');
+  imagen.src = URL.createObjectURL(image);
+  imagen.alt = "portada peliculas";
+}
+getPeticion()
--- js/script2.js
+++ js/script2.js
@@ -1,71 +1,34 @@
-
-// fetch('https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     arrayVacio = data;
-//   });
-
-// let arrayVacio;
-
-
-
-
-
-let data;
-
-async function getJSON() {
-  try {
-    const response = await fetch('https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json');
-    data = await response.json();
-  } catch (error) {
-    console.error(message.error)
-  }
+function getJSON() {
+  fetch('https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json')
+    .then(res => res.json())
+    .then(data => {
+      getQuestions(data)
+    })
 }
 
 getJSON()
 
-
-
-//Pruebas
-
-// let arrayVacio = [];
-
-// async function getJsonQuestions() {
-//   const response = await fetch('https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json')
-
-//   let json = await response.json()
-//   arrayVacio.push(json)
-// }
-
-// getJsonQuestions()
-
-// console.log(arrayVacio);
-
-
-
-// async function getJsonQuestions() {
-//   try {
-//     const response = await fetch('https://gist.githubusercontent.com/bertez/2528edb2ab7857dae29c39d1fb669d31/raw/4891dde8eac038aa5719512adee4b4243a8063fd/quiz.json')
-//     let json = await response.json()
-//     console.log(json);
-//     return json;
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-// console.log(getJsonQuestions());
-
+function getQuestions(questions) {
+  const question = questions.map((element, i) => {
+    return {
+      "question": element.question,
+      "answer": element.answers,
+      "correct": element.correct,
+      "index": i
+    }
+  });
+  console.log(question);
+}
 
 
 //Recuperar imagen
 
-async function getPeticion() {
-  const response = await fetch('https://raw.githubusercontent.com/Cjavierlopez/REP-DeliveriesHAB/main/projectONE/questions-images/question01.png');
-  const peticion = await response.blob();
+// async function getPeticion() {
+//   const response = await fetch('https://raw.githubusercontent.com/Cjavierlopez/REP-DeliveriesHAB/main/projectONE/questions-images/question01.png');
+//   const peticion = await response.blob();
 
-  const imagen = document.querySelector('#imagen');
-  imagen.src = URL.createObjectURL(peticion);
-  imagen.alt = "portada peliculas";
-}
-getPeticion()
-
+//   const imagen = document.querySelector('#imagen');
+//   imagen.src = URL.createObjectURL(peticion);
+//   imagen.alt = "portada peliculas";
+// }
+// getPeticion()
