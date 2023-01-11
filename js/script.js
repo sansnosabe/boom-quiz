04b3acaff1ef4501bc23ae94b86c87f668029f6c
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-01-11 13:32:57 +0100

    change stye and delete banners

--- js/script.js
+++ js/script.js
@@ -11,55 +11,55 @@ async function getJSON() {
 
 async function iterateArray() {
   const jsonArray = await getJSON();
+  if (jsonArray) {
+    const value = jsonArray.map((entries, i) => {
+      const question = entries.question
+      const answers = entries.answers
+      const indexQuestions = i
+      return { question, answers, indexQuestions }
+    });
 
-  const value = jsonArray.map((entries, i) => {
-    const question = entries.question
-    const answers = entries.answers
-    const indexQuestions = i
-    return { question, answers, indexQuestions }
-  })
+    const section = document.querySelector("section")
+    if (section) {
+      value.forEach(element => {
+        const index = Object.keys(jsonArray).findIndex(key => jsonArray[key].question === element.question)
 
-  const section = document.querySelector("section")
+        if (element.indexQuestions === index) {
+          const article = document.createElement('article')
+          section.appendChild(article)
 
-  value.forEach(element => {
-    const index = Object.keys(jsonArray).findIndex(key => jsonArray[key].question === element.question)
+          const header = document.createElement('header')
+          article.appendChild(header)
 
-    if (element.indexQuestions === index) {
-      const article = document.createElement('article')
-      section.appendChild(article)
+          const main = document.createElement('main')
+          article.appendChild(main)
 
-      const header = document.createElement('header')
-      article.appendChild(header)
+          const questionTitleH2 = document.createElement('h2')
+          header.appendChild(questionTitleH2)
+          questionTitleH2.textContent = element.question
 
-      const main = document.createElement('main')
-      article.appendChild(main)
+          const divQuestions = document.createElement('div')
+          main.appendChild(divQuestions).classList.add("questions")
 
-      const questionTitleH2 = document.createElement('h2')
-      header.appendChild(questionTitleH2)
-      questionTitleH2.textContent = element.question
+          const figure = document.createElement('figure')
+          main.appendChild(figure)
 
-      const divQuestions = document.createElement('div')
-      main.appendChild(divQuestions).classList.add("questions")
+          const indexImage = index.toString().padStart(2, '0');
 
-      const figure = document.createElement('figure')
-      main.appendChild(figure)
+          const image = document.createElement('img')
+          image.src = `img/questions-images/question-${indexImage}.png`;
+          image.alt = "caratula de pelicula";
+          figure.appendChild(image)
 
-      const indexImage = index.toString().padStart(2, '0');
-
-      const image = document.createElement('img')
-      image.src = `img/questions-images/question-${indexImage}.png`;
-      image.alt = "caratula de pelicula";
-      figure.appendChild(image)
-
-
-
-      element.answers.forEach(answer => {
-        const buttonAnswers = document.createElement('button')
-        buttonAnswers.textContent = answer
-        divQuestions.appendChild(buttonAnswers)
+          element.answers.forEach(answer => {
+            const buttonAnswers = document.createElement('button')
+            buttonAnswers.textContent = answer
+            divQuestions.appendChild(buttonAnswers)
+          })
+        }
       })
     }
-  })
+  }
 }
 iterateArray()
 
